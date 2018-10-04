dc56d86db0a67f7a7672ccdceeb2a6d83a40fef5
Rbusy <rbusy@localhost.localdomain>
2018-10-04 22:15:32 +0200

    Push day

---
+++ Semaine_1/Day_09/journalist.rb
@@ -0,0 +1,98 @@
+def length_of_tableau(tableau)
+	length_of_array = tableau.length
+	puts "La taille de l'array est de #{length_of_array}."
+end
+
+def contain_num(tableau)
+	result = 0
+	tableau.each do |item|
+		if item.include?("0") || item.include?("1") || item.include?("2") || item.include?("3") || item.include?("4") || item.include?("5") || item.include?("6") || item.include?("7") || item.include?("8") || item.include?("9")
+			result += 1
+		end
+	end
+	puts "Le nombre de handle qui contiennent un numéro est de #{result}."
+end
+
+def contain_aude(tableau)
+	result = 0
+	tableau.each do |item|
+		if item.include?("Aude") || item.include?("AUDE") || item.downcase.include?("aude")
+			result += 1
+		end
+	end
+	puts "Le nombre de handle qui contennent les lettres \"Aude\" est de #{result}."
+end
+
+def start_with_upper(tableau)
+	result = 0
+
+	tableau.each do |item|
+		i = 1
+		while item.slice(i) == "_" || item.slice(i) == "-"
+			i += 1
+		end
+		if item.slice(i) == item.slice(i).upcase
+			result += 1
+		end
+	end
+	puts "Le nombre de handle qui commencent par une majuscule est de #{result}."
+end
+
+def contain_maj(tableau)
+  d = tableau.grep(/[A-Z]/).count
+    puts "Le nombre de handle qui contiennent des majuscules est de #{d}."
+end
+
+def contain_underscore(tableau)
+	result = 0
+	tableau.each do |item|
+		item.each_char do |c|
+			if c == '_'
+				result += 1
+			end 
+		end
+	end
+	puts "Le nombre total d'undescore est de #{result}."
+end
+
+def sort(tableau)
+
+puts tableau.sort_by{ |x| x.downcase }
+
+end
+
+def application
+	tableau = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+	no_answer = 0
+
+	while no_answer == 0 do
+	puts "Salut!"
+	puts "Copie colle une question ou bien tape 1/2/3/4/5/6/7"
+		user_choice = gets.chomp
+		puts ''
+		if user_choice == "Combien y a t'il de journalistes dans cet array ?" || user_choice == "1"
+			length_of_tableau(tableau)
+			no_answer = 1
+		elsif user_choice == "Combien d'handle contiennent un numéro ?" || user_choice == "2"
+			contain_num(tableau)
+			no_answer = 1
+		elsif user_choice == "Combien d'handle contiennent les 4 lettres du prénom \"Aude\" à la suite (sans faire attention à la casse) ?" || user_choice == "3" 
+			contain_aude(tableau)
+			no_answer = 1
+		elsif user_choice == "Combien commencent par une majuscule ?" || user_choice == "4"
+			start_with_upper(tableau)
+			no_answer = 1
+		elsif user_choice == "Combien contiennent une majuscule ?" || user_choice == "5"
+			contain_maj(tableau)
+			no_answer = 1
+		elsif user_choice == "Combien de _ dans tous les pseudos confondus ?" || user_choice == "6"
+			contain_underscore(tableau)
+			no_answer = 1
+		elsif user_choice == "Trie la liste de handle par ordre alphabétique" || user_choice == "7"
+			sort(tableau)
+			no_answer = 1
+		end
+	end
+end
+
+application
